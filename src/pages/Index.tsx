caa30891b7c1aeda2635a5bfce1a1af1cba23587
zan4yov <razancollege06@gmail.com>
2025-09-10 10:44:31 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -15,7 +15,7 @@ import {
   Phone,
   MessageCircle,
 } from 'lucide-react';
-import Link from 'next/link';
+
 
 const Index = () => {
   const [currentTestimonial, setCurrentTestimonial] = useState(0);
@@ -133,13 +133,14 @@ const Index = () => {
                   <MessageCircle className="w-5 h-5" />
                   Pesan Sekarang
                 </a>
-                <Link
-  href="/menu"
-  className="btn-ghost text-white border-white/50 hover:bg-white/10 justify-center sm:justify-start"
->
-  Lihat Menu
-  <ArrowRight className="w-4 h-4 ml-2" />
-</Link>
+                <a
+                    href="/menu"
+                      className="btn-ghost text-white border-white/50 hover:bg-white/10 justify-center sm:justify-start"
+                >
+                          Lihat Menu
+                <ArrowRight className="w-4 h-4 ml-2" />
+                </a>
+
 
               {/* Social Proof (hapus rating & pengalaman, pakai count-up) */}
               <div className="flex items-center space-x-8 pt-4">
